da63b11626446be6dcd23e04e7ae0445ea8c445f
Carlos Simon Villas <csimon@murdock.es>
2015-07-12 19:02:25 +0200

    Initial Commit

---
+++ Palette-iOS/Palette-iOS.h
@@ -0,0 +1,19 @@
+//
+//  Palette-iOS.h
+//  Palette-iOS
+//
+//  Created by Carlos Simon Villas on 12/07/15.
+//  Copyright (c) 2015 Charlisim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Palette-iOS.
+FOUNDATION_EXPORT double Palette_iOSVersionNumber;
+
+//! Project version string for Palette-iOS.
+FOUNDATION_EXPORT const unsigned char Palette_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Palette_iOS/PublicHeader.h>
+
+
---
+++ Palette-iOSTests/Palette_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Palette_iOSTests.swift
+//  Palette-iOSTests
+//
+//  Created by Carlos Simon Villas on 12/07/15.
+//  Copyright (c) 2015 Charlisim. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Palette_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
