f9add413b7582c43d9d56c48519f8058fae82c64
Carlos Simon Villas <csimon@murdock.es>
2015-07-12 20:38:10 +0200

    Initial Commit

---
+++ Palette/Palette.h
@@ -0,0 +1,19 @@
+//
+//  Palette.h
+//  Palette
+//
+//  Created by Carlos Simon Villas on 12/07/15.
+//  Copyright (c) 2015 Charlisim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Palette.
+FOUNDATION_EXPORT double PaletteVersionNumber;
+
+//! Project version string for Palette.
+FOUNDATION_EXPORT const unsigned char PaletteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Palette/PublicHeader.h>
+
+
